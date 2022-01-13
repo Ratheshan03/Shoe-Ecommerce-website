9a69efcf161ba0799d22969fb91773b3eb19526d
Ratheshan03 <lionratheshan@gmail.com>
2022-01-13 11:08:31 +0530

    initial-size chart

--- packages/js/main.js
+++ packages/js/main.js
@@ -33,6 +33,21 @@ $(document).ready(function () {
     $("#tabs").tabs();
   });
 
+  $("#wmyTab a").click(function (e) {
+    e.preventDefault();
+    this.tab("show");
+  });
+
+  $("#ladies").click(function () {
+    document.getElementById("man").style.display = "none";
+    document.getElementById("woman").style.display = "block";
+  });
+
+  $("#dudes").click(function () {
+    document.getElementById("man").style.display = "block";
+    document.getElementById("woman").style.display = "none";
+  });
+
   //* Retrieving Results *//
   $("#submit-btn").on("click", function (e) {
     var style = $("#style").val();
